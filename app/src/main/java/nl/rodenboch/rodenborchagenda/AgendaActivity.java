39cc06fe512c188179bf13feacf3318a87f0552b
Maarten Siersema <maarten.siersema@freshheads.com>
2019-02-12 20:15:19 +0100

    added listview

--- app/src/main/java/nl/rodenboch/rodenborchagenda/AgendaActivity.java
+++ app/src/main/java/nl/rodenboch/rodenborchagenda/AgendaActivity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class AgendaActivity extends AppCompatActivity {
 
@@ -14,6 +18,17 @@ public class AgendaActivity extends AppCompatActivity {
 
         setSupportActionBar ((Toolbar) findViewById(R.id.toolbar));
         getSupportActionBar().setDisplayHomeAsUpEnabled (true);
+
+        ArrayList<String> news = new ArrayList<String>(){{
+                    add("titel 1");
+                    add("titel 2");
+        }};
+
+        ListView listview = findViewById(R.id.listview);
+
+        final ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, news);
+        listview.setAdapter(adapter);
     }
 
     @Override
