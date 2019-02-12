8929f347d703d40820c57a7c628b752de9425b14
Maarten Siersema <maarten.siersema@freshheads.com>
2019-02-12 17:58:08 +0100

    added toolbar with back navigation

--- app/src/main/java/nl/rodenboch/rodenborchagenda/AgendaActivity.java
+++ app/src/main/java/nl/rodenboch/rodenborchagenda/AgendaActivity.java
@@ -2,6 +2,8 @@ package nl.rodenboch.rodenborchagenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class AgendaActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class AgendaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agenda);
+
+        setSupportActionBar ((Toolbar) findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled (true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
