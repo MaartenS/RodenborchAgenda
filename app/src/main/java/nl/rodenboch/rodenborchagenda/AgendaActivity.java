0edb889e9349d421fc255916513a4379e87c9f4f
Maarten Siersema <maarten.siersema@freshheads.com>
2019-02-12 21:20:27 +0100

    Revert "added toolbar with back navigation"
    This reverts commit 8929f34

--- app/src/main/java/nl/rodenboch/rodenborchagenda/AgendaActivity.java
+++ app/src/main/java/nl/rodenboch/rodenborchagenda/AgendaActivity.java
@@ -2,12 +2,6 @@ package nl.rodenboch.rodenborchagenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
 
 public class AgendaActivity extends AppCompatActivity {
 
@@ -15,30 +9,5 @@ public class AgendaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agenda);
-
-        setSupportActionBar ((Toolbar) findViewById(R.id.toolbar));
-        getSupportActionBar().setDisplayHomeAsUpEnabled (true);
-
-        ArrayList<String> news = new ArrayList<String>(){{
-                    add("titel 1");
-                    add("titel 2");
-        }};
-
-        ListView listview = findViewById(R.id.listview);
-
-        final ArrayAdapter adapter = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1, news);
-        listview.setAdapter(adapter);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == android.R.id.home) {
-            finish();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
